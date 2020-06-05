373ad6f1aa3f8965854e20db68fad50632f0747a
adam24598 <64758097+adam24598@users.noreply.github.com>
2020-06-05 09:40:31 +0200

    2nd

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import "./style.css"
-import imageInSrc from "./imageInSrc.jpg";
+import imageInSrc from "./imageInSrc.png";
 function App() {
   return (
     <div style={{border:'solid 1px black',maxWidth:"100vw"}}>  
-          	<h1 className="title red"> Your name here </h1>  
+          	
+            <div><h1 className="title red"> Your name here </h1>  </div>
             <div className='new-line'></div>
  
-            <img src={imageInSrc.jpg}/>            
+            <div><img src={imageInSrc.jpg} alt="erreur"/>   </div>         
              <div className='new-line'></div>
 
  
-             	<img src="/imageInPublic.jpg"/> 
+             <div>	<img src="/imageInPublic.jpg" alt="erreur"/> </div>
  
     </div>  
- 
+ <div>
     <video width="320" height="240" controls>  
 
         <source src="myVideo.mp4" type="video/mp4"/> 
  
     </video>
+    </div>
   );
 }
 
